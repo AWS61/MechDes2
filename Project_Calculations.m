a4c71cb452a2e0fbdc471c54a4b52aad45dd45ed
CarsonHertrick <cjh197@pitt.edu>
2026-03-03 14:58:40 -0500

    Update Project_Calculations.m
    Added stress analysis for pulleys and shafts with pulleystress function and gearstress function. Reaction forces are in here Ill clean it up more later

--- Project_Calculations.m
+++ Project_Calculations.m
@@ -133,5 +133,104 @@ Facewidth = Kv_milled*Wt/(Sy*Module*Y)
 
 %% Support Shafts and Pullies
 
+pullstress = pulleyshaft(Tensile_Force);
+[maxstress3, maxstress4] = gearshaft(Ft54, Fr54, Ft23, Fr23, T3, T_in, F_in, Tensile_Force, r_Drum);
+
+function [maxstress3, maxstress4] = gearshaft(ft54, fr54, ft23, fr23, T3, T_in, F_in, Tensile_force, r_Drum)
+    
+    % shaft lengths and 
+    l23 = 40*10^-3;   
+    l34 = 30*10^-3;
+    l45 = 40*10^-3;
+    lcrank = 5*10^-3;
+    d4 = 12*10^-3;
+    d3 = 15*10^-3;
+    l = l23 + l34 + l45; % total length
+    
+    % Reaction forces on gear shafts 2
+    Roy = (ft54*(l23+l34)-ft23*l23)/(l);   
+    Rby = ft23+Roy-ft54;
+    Roz = (fr54*(l23+l34)-fr23*l23)/(l);
+    Rbz = fr23+Roz-fr54;
+    
+    %Reaction forces on gear shaft 1
+    R2oy = (ft23*l23-F_in*lcrank)/(l);
+    R2by = R2oy - ft54 + Tensile_force;
+    R2bz = fr23*(l45+l34)/l;
+    R2oz = fr23-R2bz;
+    % moments for gear shaft 1
+    My2 = R2oy*(l23+l34);
+    Mz2 = R2oz*(l23+l34);
+
+    % moments for shaft 2
+    My4 = Roy*l45;  % or  whichever is bigger
+    My3 = Rby*l23;
+    Mz4 = Roz*l45;  % or 
+    Mz3 = Rbz*l23;
+
+    % forces on gear shaft 3
+    R3by = (-ft54*l45+Tensile_force*(l34+l45))/l;
+    R3oy = ft54+R3by-Tensile_force;
+    R3bz = (fr54*l45)/l;
+    R3oz = fr54-R3bz;
+    T_out = Tensile_force*r_Drum;
+
+    %moments in shaft 3
+    My5 = R3oy*l45;
+    Mz5 = R3oz*l45;
+    MyD = R3by*l23;
+    MzD = R3bz*l23;
+
+
+    % Moments at each gear or drum
+    M2 = sqrt(My2^2+Mz2^2);
+    M3 = sqrt(My3^2+Mz3^2);
+    M4 = sqrt(My4^2+Mz4^2);
+    M5 = sqrt(My5^2+Mz5^2);
+    MD = sqrt(MyD^2+MzD^2);
+
+   
+    
+    % prelinary stress concentrations for 
+    gearKt = 4;
+    gearKts = 4;
+
+    maxstress2 = (sqrt((32*gearKt*M2/(pi*d4^3))^2+3*(16*gearKts*T_in/(pi*d4^3))))*10^-6;
+    maxstress3 = (sqrt((32*gearKt*M3/(pi*d3^3))^2+3*(16*gearKts*T3/(pi*d3^3))))*10^-6;
+    maxstress4 = (sqrt((32*gearKt*M4/(pi*d4^3))^2+3*(16*gearKts*T3/(pi*d4^3))))*10^-6;
+    maxstress5 = (sqrt((32*gearKt*M5/(pi*d4^3))^2+3*(16*gearKts*T_out/(pi*d4^3))))*10^-6;
+    
+    % solving for minimum shaft diamater
+    syms d
+    assume(d >= 0)
+    maxstressD = (sqrt((32*gearKt*MD/(pi*d^3))^2+3*(16*gearKts*T_out/(pi*d^3))))*10^-6 == 310/2;
+    Min_d = solve(maxstressD, d, Real=true);
+    dmin = double(Min_d);
+
+    disp(['The minimum shaft diameter is ', num2str(dmin*10^3), ' [mm]'])
+    
+    disp(['von Mises stress at gear 2 = ', string(maxstress2), '[MPa]'])
+    disp(['von Mises stress at gear 3 = ', string(maxstress3), '[MPa]'])
+    disp(['von Mises stress at gear 4 = ', string(maxstress4), '[MPa]'])
+    disp(['von Mises stress at gear 5 = ', string(maxstress5), '[MPa]'])
+    disp(['with a shaft length of ', num2str(l)])
+end
+
+    %% pulley shaft analysis
+
+function [pulstress] = pulleyshaft(t)
+    t = 1000/(.985^3);  %pulley tension in N
+    pulKt = 1.0; % pulley stess concentration
+    pullen = 1; % length of pulley shaft in m
+    puld = .0254*1.3; % pulley shaft diameter
+    
+    % maximum pulley stress
+    pulstress = (32*pulKt*t*(pullen/2)/(pi*puld^3))*10^-6;
+    
+    disp(["The maximum pulley shaft stress is ",num2str(pulstress), " MPa"])
+end
+
+
+
 
 %% Bearings
